95920a28022378fa9b6dba275666d9f21a158857
Thulasi Ram <thulasi503@gmail.com>
2023-09-08 19:54:18 +0530

    feat: initial setup

---
+++ src/main/java/com/ff/intg/FfIntgApplication.java
@@ -0,0 +1,29 @@
+package com.ff.intg;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class FfIntgApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FfIntgApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Beans:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
+
+}
---
+++ src/test/java/com/ff/intg/FfIntgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ff.intg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfIntgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
